7cbdb6e234da4cf56cfced2a02cc75f5022f2809
suraj-sakhare <suraj.sakhare@payppy.co>
2026-01-10 14:13:47 +0530

    Test: Debug true

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c*$-&67*&!mmwy)k*+9&aomkj^r6-+fb1^v^giw8a)ao)6%&v_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".onrender.com",]
 
